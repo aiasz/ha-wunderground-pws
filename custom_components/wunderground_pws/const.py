097bf40f7ef88d9c59e0f26aeca788a7769e81d5
aiasz <67366264+aiasz@users.noreply.github.com>
2026-02-27 12:41:24 +0100

    Update const.py - API-based constants (v1.1.0)

--- custom_components/wunderground_pws/const.py
+++ custom_components/wunderground_pws/const.py
@@ -1,28 +1,40 @@
-"""Constants for Wunderground PWS integration."""
+"""Constants for Wunderground PWS integration.
+
+Keszito: Aiasz
+Verzio: 1.1.0
+"""
 
 DOMAIN = "wunderground_pws"
 
-DEFAULT_URL = "https://www.wunderground.com/dashboard/pws/IKAPOS27"
+DEFAULT_STATION_ID = "IKAPOS27"
 DEFAULT_SCAN_INTERVAL = 5  # minutes
 MIN_SCAN_INTERVAL = 1
 MAX_SCAN_INTERVAL = 60
 
-CONF_PWS_URL = "pws_url"
+WU_API_URL = "https://api.weather.com/v2/pws/observations/current"
+
+CONF_STATION_ID = "station_id"
+CONF_API_KEY = "api_key"
 CONF_SCAN_INTERVAL = "scan_interval"
 
 ATTR_TEMPERATURE = "temperature"
+ATTR_FEELS_LIKE = "feels_like"
+ATTR_DEW_POINT = "dew_point"
 ATTR_HUMIDITY = "humidity"
 ATTR_PRESSURE = "pressure"
 ATTR_WIND_SPEED = "wind_speed"
 ATTR_WIND_GUST = "wind_gust"
 ATTR_WIND_BEARING = "wind_bearing"
+ATTR_WIND_COMPASS = "wind_compass"
 ATTR_PRECIPITATION = "precipitation"
 ATTR_PRECIPITATION_RATE = "precipitation_rate"
 ATTR_SOLAR_RADIATION = "solar_radiation"
 ATTR_UV_INDEX = "uv_index"
-ATTR_DEW_POINT = "dew_point"
-ATTR_FEELS_LIKE = "feels_like"
-ATTR_VISIBILITY = "visibility"
-ATTR_CONDITION = "condition"
 ATTR_STATION_ID = "station_id"
 ATTR_LAST_UPDATED = "last_updated"
+ATTR_LAT = "lat"
+ATTR_LON = "lon"
+ATTR_LOCATION_NAME = "location_name"
+ATTR_COUNTRY = "country"
+ATTR_ELEVATION_M = "elevation_m"
+ATTR_CONDITION = "condition"
